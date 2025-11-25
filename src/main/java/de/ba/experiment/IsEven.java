677bbf5cbed8c0436797806e234f6ad0e97b61f3
romeoS16 <romeoschweizer29@gmail.com>
2025-11-25 18:58:49 +0100

    javadoc

--- src/main/java/de/ba/experiment/IsEven.java
+++ src/main/java/de/ba/experiment/IsEven.java
@@ -1,9 +1,14 @@
 package de.ba.experiment;
 
 public class IsEven {
+
     /**
      * Gibt true zurück, wenn n gerade ist, sonst false.
+     *
+     * @param n ganze Zahl, die geprüft werden soll
+     * @return true, wenn n gerade ist, sonst false
      */
+
     public static boolean isEven(int n) {
         // TODO: implementieren
         return false;
